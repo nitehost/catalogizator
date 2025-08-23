43fb8764267cf3a35d223d82a54971e456069e86
nitehost <nitehost@yandex.ru>
2025-08-23 15:23:56 +0300

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use gtk4 as gtk;
-use gtk::prelude::*;
 use gtk::glib;
+use gtk::prelude::*;
+use gtk4 as gtk;
 
 const APP_ID: &str = "ru.nitehost.Catalogizator";
 const WIN_ID: &str = "CatalogizatorApp";
@@ -8,32 +8,29 @@ const UI_FILE: &str = "res/window.ui";
 const DATABASE: &str = "db/collections.db";
 
 mod collection;
+mod entry;
 mod group;
 mod status;
-mod entry;
 mod template;
 
 fn main() -> glib::ExitCode {
-
     // приложение
-    let app = gtk::Application::builder()
-        .application_id(APP_ID)
-        .build();
+    let app = gtk::Application::builder().application_id(APP_ID).build();
 
     // присоединение к сигналу activate
-    // app.connect_activate(on_activate);
+    app.connect_activate(on_activate);
 
     // запуск приложения
     app.run()
 }
 
 fn on_activate(app: &gtk::Application) {
-
     // загружаем окно из UI-файла
     let builder = gtk::Builder::from_file(UI_FILE);
 
     // получаем из UI объект окна
-    let window: gtk::ApplicationWindow = builder.object(WIN_ID)
+    let window: gtk::ApplicationWindow = builder
+        .object(WIN_ID)
         .expect("No GtkApplicationWindow object in UI-file");
 
     // устанавливаем связь окна с приложением
