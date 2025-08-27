133eb1dc55c8b4f3b58d1d29d0b9c56dde08dbb0
nitehost <nitehost@yandex.ru>
2025-08-27 19:33:03 +0300

    Добавлено поле "view" для коллекций с возможными вариантами выбора List и Grid.

--- src/collection.rs
+++ src/collection.rs
@@ -1,10 +1,27 @@
 use gtk4 as gtk;
+use std::fmt;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 #[derive(Debug)]
+enum EntryView {
+    List,
+    Grid,
+}
+
+impl fmt::Display for EntryView {
+    fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
+        match self {
+            EntryView::List => write!(f, "list"),
+            EntryView::Grid => write!(f, "grid"),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct CatalogCollection {
     id: u32,
+    view: EntryView,
     name: String,
 }
 
@@ -12,6 +29,10 @@ impl CatalogCollection {
 
     pub fn get_id(&self) -> &u32 {
         &self.id
+    }
+
+    pub fn get_view(&self) -> &EntryView {
+        &self.view
     }
 
     pub fn get_name(&self) -> &String {
@@ -24,7 +45,7 @@ pub fn get_collections() -> Result<Vec<CatalogCollection>> {
 
     let conn = rusqlite::Connection::open(crate::DATABASE)?;
     
-    let mut stmt = conn.prepare("SELECT id, name FROM collections")?;
+    let mut stmt = conn.prepare("SELECT id, view, name FROM collections")?;
     let mut rows = stmt.query([])?;
         
     let mut collections:Vec<CatalogCollection> = vec![];
@@ -32,7 +53,11 @@ pub fn get_collections() -> Result<Vec<CatalogCollection>> {
         collections.push(
             CatalogCollection {
                 id: row.get(0)?,
-                name: row.get(1)?,
+                view: match row.get::<usize, String>(1)?.as_str() {
+                    "grid" => EntryView::Grid,
+                    _ => EntryView::List,
+                },
+                name: row.get(2)?,
             }
         );
     }
